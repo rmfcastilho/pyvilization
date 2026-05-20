45899598db27280009d41e6b1e73549d8e4be7a3
Rodrigo M. F. Castilho <rodrigo.m.castilho@outlook.com>
2026-05-19 21:07:08 -0300

    refactor: civilization model

--- models/base/civilization.py
+++ models/base/civilization.py
@@ -1,18 +1,20 @@
-class Civilization(self, name, modifiers, resources, wonders, cities):
-    def __init__(self):
+class Civilization:
+    def __init__(self, name, modifiers, resources, wonders, cities):
         self.name = name
         self.modifier = modifiers
         self.resources = resources
         self.wonders = wonders
         self.cities = cities
 
-    def found_city():
-        pass
+    def found_city(self):
+        self.cities.append(get_standard_city_name())
     
-    def obtain_city(city):
-        cities.append(city)
+    def obtain_city(self, city):
+        self.cities.append(city)
     
-    def remove_city(city)
-        cities.remove(city)
+    def remove_city(self, city):
+        self.cities.remove(city)
 
-    
+def get_standard_city_name():
+    print("Don't forget to implement this")
+    return 'IMPLEMENT THIS'
